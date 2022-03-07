d592561c4fd122f21a5deb4e0532dfe8d458ce11
berke-horozal-crea <90218881+berke-horozal-crea@users.noreply.github.com>
2022-03-07 23:35:21 +0300

    Initial commit of word-finder frontend w react

---
+++ word-finder-frontend/src/index.js
@@ -0,0 +1,71 @@
+import './index.css';
+
+
+let lengthInput = document.getElementById('length');
+lengthInput.onkeyup = (e) => {
+  let word = document.getElementById('word');
+  word.innerHTML = "";
+
+  for (let i = 0; i < e.target.value; i++) {
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.className = "word-textbox";
+    input.id = "word-textbox-" + i;
+    input.maxLength = 1;
+    input.onkeydown = (e) => (e.key === "Backspace" || e.key === "Delete") || /[a-z ğüşiöçĞÜŞİÖÇ]/gi.test(e.key);
+    word.appendChild(input);
+  }
+}
+
+
+var myHeaders = new Headers();
+// console.log({process.env.REACT_APP_AUTHORIZATION})
+myHeaders.append("Authorization", process.env.REACT_APP_API_KEY);
+myHeaders.append("Content-Type", "application/json");
+
+let submit = document.getElementById('submit');
+submit.onclick = function () {
+
+  let length = document.getElementById('length').value;
+  let exclude = document.getElementById('exclude').value.toUpperCase();
+  let include = document.getElementById('include').value.toUpperCase();
+  const excludeArray = Array.from(new Set(exclude.replace(/[^A-Za-z ]/g, '').split('')));
+  const includeArray = Array.from(new Set((include).replace(/[^A-Za-z ]/g, '').split('')));
+
+  const wordArray = document.getElementsByClassName('word-textbox')
+
+  let wordResult = ""
+  for (var i = 0; i < wordArray.length; i++) {
+    wordResult += wordArray[i].value == "" ? ' ' : wordArray[i].value;
+  }
+
+  console.log(wordResult)
+  var raw = JSON.stringify({
+    "exclude": excludeArray,
+    "include": includeArray,
+    "length": length,
+    "word": wordResult,
+    "language": "tr"
+  });
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  fetch("http://word-finder-get-words.herokuapp.com/word_finder/get_words", requestOptions)
+    .then(response => response.text())
+    .then(data => {
+      const result = JSON.parse(data).result;
+      const resultList = document.getElementById('resultList');
+      resultList.innerHTML = '';
+
+      result.forEach((value) => {
+        const resultItem = document.createElement('label');
+        resultItem.innerHTML = value;
+        resultList.appendChild(resultItem);
+        resultList.append(', ')
+      })
+    })
+    .catch(error => console.log('error', error));
+}
