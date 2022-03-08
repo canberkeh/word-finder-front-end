27a59ac275fd2c4579852360b227aea2ed778ca2
berke-horozal-crea <90218881+berke-horozal-crea@users.noreply.github.com>
2022-03-08 12:24:55 +0300

    Fix include request error. Change string to lowercase.

--- src/index.js
+++ src/index.js
@@ -27,32 +27,34 @@ let submit = document.getElementById('submit');
 submit.onclick = function () {
 
   let length = document.getElementById('length').value;
-  let exclude = document.getElementById('exclude').value.toUpperCase();
-  let include = document.getElementById('include').value.toUpperCase();
+  let exclude = document.getElementById('exclude').value.toLowerCase();
+  let include = document.getElementById('include').value.toLowerCase();
   const excludeArray = Array.from(new Set(exclude.replace(/[^A-Za-z ]/g, '').split('')));
   const includeArray = Array.from(new Set(include.replace(/[^A-Za-z ]/g, '').split('')));
+  
 
-  const wordArray = document.getElementsByClassName('word-textbox')
+  const wordArray = document.getElementsByClassName('word-textbox');
 
   let wordResult = ""
   for (var i = 0; i < wordArray.length; i++) {
     wordResult += wordArray[i].value == "" ? ' ' : wordArray[i].value;
   }
 
-  console.log(wordResult)
   var raw = JSON.stringify({
-    "exclude": excludeArray,
     "include": includeArray,
+    "exclude": excludeArray,
     "length": length,
     "word": wordResult,
     "language": "tr"
   });
+
   var requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
     redirect: 'follow'
   };
+
   fetch("https://word-finder-get-words.herokuapp.com/word_finder/get_words", requestOptions)
     .then(response => response.text())
     .then(data => {
@@ -65,7 +67,7 @@ submit.onclick = function () {
         resultItem.innerHTML = value;
         resultList.appendChild(resultItem);
         resultList.append(', ')
-      })
+      }) 
     })
     .catch(error => console.log('error', error));
 }
